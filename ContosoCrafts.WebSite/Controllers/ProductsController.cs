9f21106f6fb685dbd14446b613313c955c059e94
cojua8 <34746495+cojua8@users.noreply.github.com>
2021-07-04 00:19:46 -0400

    Add project files.

---
+++ ContosoCrafts.WebSite/Controllers/ProductsController.cs
@@ -0,0 +1,33 @@
+﻿using ContosoCrafts.WebSite.Models;
+using ContosoCrafts.WebSite.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContosoCrafts.WebSite.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        public JsonFileProductsService ProductsService { get; set; }
+        public ProductsController(JsonFileProductsService productsService)
+        {
+            ProductsService = productsService;
+        }
+
+        [HttpGet]
+        public IEnumerable<Product> GetProducts() => ProductsService.GetProducts();
+
+        [Route("rate")]
+        [HttpGet]
+        public ActionResult Get([FromQuery] string productId, [FromQuery] int rating)
+        {
+            ProductsService.AddRating(productId, rating);
+            return Ok();
+        }
+    }
+}
---
+++ ContosoCrafts.WebSite/Pages/Index.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using ContosoCrafts.WebSite.Models;
+using ContosoCrafts.WebSite.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContosoCrafts.WebSite.Pages
+{
+	public class IndexModel : PageModel
+	{
+		private readonly ILogger<IndexModel> _logger;
+		private JsonFileProductsService _productsService;
+		public IEnumerable<Product> Products { get; private set; }
+
+		public IndexModel(ILogger<IndexModel> logger, JsonFileProductsService productsService)
+		{
+
+			_logger = logger;
+			_productsService = productsService;
+		}
+
+		public void OnGet()
+		{
+			Products = _productsService.GetProducts();
+			_logger.LogInformation($"Got {Products.Count()} products");
+		}
+	}
+}
